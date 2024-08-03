e7233af624656bea15a0ddec17116eb3f121495e
Shahbaj Sheikh <thisisshahbaj@gmail.com>
2024-08-03 13:32:05 +0530

    add feature

--- src/assets/components/Sidebar.jsx
+++ src/assets/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Stack } from "@mui/material";
 import { categories } from "../utils/constants";
+import { Rowing } from "@mui/icons-material";
 
 
 const Sidebar = ({selectedCategory, setSelectedCategory}) => {
@@ -20,8 +21,9 @@ const Sidebar = ({selectedCategory, setSelectedCategory}) => {
         onClick={() => {setSelectedCategory(category.name)}}
         style={{
           background: category.name === selectedCategory && "rgb(3, 103, 203)",
-          color: "white",
-        }}
+          color: "white"
+        }
+        }
         key={category.name}
       >
         <span style={{ color: category.name === selectedCategory ? "white" : "rgb(96, 167, 237)", marginRight: "15px" }}>
@@ -30,6 +32,7 @@ const Sidebar = ({selectedCategory, setSelectedCategory}) => {
         <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
           {category.name}
         </span>
+        
       </button>
     ))}
   </Stack>
